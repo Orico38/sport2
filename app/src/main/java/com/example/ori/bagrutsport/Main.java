b912fc1af15997b9db2e20f7922177a51234c9c3
Orico38 <ori.cohen380111@gmail.com>
2018-05-15 23:13:14 +0300

    End

--- app/src/main/java/com/example/ori/bagrutsport/Main.java
+++ app/src/main/java/com/example/ori/bagrutsport/Main.java
@@ -1,68 +1,125 @@
 package com.example.ori.bagrutsport;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-public class Main extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    Spinner spinner;
-    int i=10;
-    String[]arry=new String [4];
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ServiceConfigurationError;
+import java.util.zip.CheckedOutputStream;
+
+public class Main extends AppCompatActivity {
+
+    private static final String FILENAME = "";
+    EditText ET;
+    String name3;
+    FileOutputStream fos;
+    OutputStreamWriter osw;
+    AlertDialog.Builder adb;
+    InputStream is;
+    InputStreamReader tmp;
+    BufferedReader reader;
+    String str;
+    StringBuffer buffer;
+    BufferedWriter bw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner) (findViewById(R.id.spinner));
-        arry[0]="כפיפות בטן";
-        arry[1]="שכיבות סמיכה";
-        arry[2]="מתח";
-        arry[3]="מרפקים";
+        ET=(EditText) findViewById(R.id.name1);
+        adb=new AlertDialog.Builder(this);
+        adb.setTitle("שגיאה");
+        adb.setMessage("לא הכנסת את שמך");
+        adb.setPositiveButton("הבנתי", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
-        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, arry);
-        adp.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spinner.setAdapter(adp);
-        spinner.setOnItemSelectedListener(this);
-    }
-    public boolean onCreateOptionsMenu (android.view.Menu menu){
+            }
+        });
 
-        getMenuInflater().inflate(R.menu.main , menu);
-        return true;
+
     }
-    public boolean onOptionsItemSelected(MenuItem item){
-       String st=item.getTitle().toString();
-        if(st.equals("קרדיטים")){
-            Intent credits= new Intent(this, Credits.class);
-            startActivity(credits);
+
+
+    public void next1(View view) {
+        name3 = ET.getText().toString();
+        if (name3.equals("")) {
+            AlertDialog ad = adb.create();
+            ad.show();
+
+        } else {
+            try {
+                fos = openFileOutput(name3, Context.MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            osw = new OutputStreamWriter(fos);
+            bw = new BufferedWriter(osw);
+            try {
+                bw.write(name3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Intent t= new Intent(this, Secmain.class);
+            startActivity(t);
         }
-        return true;
 
-    }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch (position){
-            case 0:i=0;break;
-            case 1:i=1;break;
-            case 2:i=2;break;
-            case 3:i=3;break;
+        try {
+            is = openFileInput(name3);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-    }
+        tmp = new InputStreamReader(is);
+        reader = new BufferedReader(tmp);
+        buffer = new StringBuffer();
+        try {
+            while ((str = reader.readLine()) != null) {
+                buffer.append(str);
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    public void next(View view) {
-        Intent t=new Intent(this, Exs.class);
-        t.putExtra("ex",i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Intent t= new Intent(this, Secmain.class);
+        String str=new String(buffer);
+        t.putExtra("name",str);
         startActivity(t);
+
+
+
     }
-}
+    }
+
---
+++ app/src/main/java/com/example/ori/bagrutsport/Secmain.java
@@ -0,0 +1,88 @@
+package com.example.ori.bagrutsport;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
+public class Secmain extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    int i=10;
+    String str;
+    TextView tv;
+    String[]arry=new String [4];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secmain);
+
+        tv=(TextView) findViewById(R.id.hi);
+        Intent t=getIntent();
+        str=t.getStringExtra("name");
+
+
+        tv.setText("שלום "+ str+" על מה תרצה לעבוד היום");
+
+
+        spinner = (Spinner) (findViewById(R.id.spinner));
+        arry[0]="כפיפות בטן";
+        arry[1]="שכיבות סמיכה";
+        arry[2]="מתח";
+        arry[3]="מרפקים";
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, arry);
+        adp.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adp);
+        spinner.setOnItemSelectedListener(this);
+    }
+    public boolean onCreateOptionsMenu (android.view.Menu menu){
+
+        getMenuInflater().inflate(R.menu.main , menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        String st=item.getTitle().toString();
+        if(st.equals("קרדיטים")){
+            Intent credits= new Intent(this, Credits.class);
+            startActivity(credits);
+        }
+        return true;
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:i=0;break;
+            case 1:i=1;break;
+            case 2:i=2;break;
+            case 3:i=3;break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void next(View view) {
+        Intent t=new Intent(this, Exs.class);
+        t.putExtra("ex",i);
+        startActivity(t);
+    }
+}
